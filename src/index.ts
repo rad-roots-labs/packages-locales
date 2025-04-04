9b3061eeca693b94e9ba8536267302509cecf1aa
triesap <137732411+triesap@users.noreply.github.com>
2025-04-04 04:47:44 +0000

    Add/edit `en` messages, add locales.json data file.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import iso3166_2 from "./data/iso3166_2.json";
 import iso639_1 from "./data/iso639_1.json";
+import locales from "./data/locales.json";
 
-const locales = [`en`] as const;
-
+export const locales_default = [`en`] as const;
 export { iso3166_2, iso639_1, locales };
