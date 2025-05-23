193dad0032b268979be79747433a00c2288473ba
triesap <137732411+triesap@users.noreply.github.com>
2025-05-23 02:43:05 +0000

    Add/edit `en` messages. Add JSON import assertions.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-import iso3166_2 from "./data/iso3166_2.json";
-import iso639_1 from "./data/iso639_1.json";
-import locales from "./data/locales.json";
+import iso3166_2 from "./data/iso3166_2.json" assert { type: "json" };
+import iso639_1 from "./data/iso639_1.json" assert { type: "json" };
+import locales from "./data/locales.json" assert { type: "json" };
+export { iso3166_2, iso639_1, locales };
 
 export const locales_default = [`en`] as const;
-export { iso3166_2, iso639_1, locales };
+export type Locales = typeof locales_default[number];
+export const locales_set = new Set<string>(locales_default);
+
+export const get_locales_undef = (val?: string): Locales | undefined => {
+    switch (val) {
+        case `en`:
+            return val;
+        default:
+            return undefined;
+    }
+};
+
+export const get_locales = (val?: string, fallback?: Locales): Locales => {
+    const loc = get_locales_undef(val);
+    return loc || fallback || `en`;
+};
